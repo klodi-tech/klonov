69e51df21526a8078c1d264196612a6c725b1fde
klodi-tech <klodi.petre@gmail.com>
2025-11-20 00:09:45 +0000

    add player class, add circleshape, setup drawing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 from logger import log_state
@@ -12,15 +12,19 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)    
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+  
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
