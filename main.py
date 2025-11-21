b7052b02ce606b1b43e9ce4c6d8440a8e7079e14
klodi-tech <klodi.petre@gmail.com>
2025-11-21 20:52:00 +0000

    Add player movement logic with PLAYER_SPEED AND PLAYER_TURN_SPEED  and update rotation handling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from player import Player
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_TURN_SPEED
 
 from logger import log_state
 
@@ -22,9 +22,10 @@ def main():
                 return
   
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60)/ 1000
 
 if __name__ == "__main__":
     main()
